baacc362f26e8d5ee6c66aa45779637273ef20ac
chriscauley <chris@lablackey.com>
2020-05-08 00:04:59 +0000

    ConfigHook \m/

---
+++ client/ConfigHook.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import globalHook from 'use-global-hook'
+import Storage from '@unrest/storage'
+import Form from '@unrest/react-jsonschema-form'
+
+class ConfigForm extends React.Component {
+  state = {}
+  onSubmit = (formData) => this.props.config.actions.save(formData)
+  onChange = (formData) => this.setState({ formData })
+  render() {
+    const { schema, uiSchema, formData, ...props } = this.props.config
+    return (
+      <Form
+        formData={this.state.formData || formData}
+        schema={schema}
+        uiSchema={uiSchema}
+        onSubmit={this.onSubmit}
+        onChange={this.onChange}
+        {...props}
+      />
+    )
+  }
+}
+
+export default (name, { initial, schema, uiSchema, actions }) => {
+  const storage = new Storage('app_config__' + name)
+  const base_actions = {
+    save: (store, formData) => {
+      storage.set('formData', formData)
+      store.setState({ formData })
+      store.actions.onSave(formData)
+    },
+    onSave() {},
+  }
+  const makeHook = globalHook(
+    React,
+    { formData: storage.get('formData') || initial },
+    { ...base_actions, ...actions },
+  )
+
+  const connect = (Component, { propName = 'config' } = {}) => {
+    return function ConfigProvider(props) {
+      const [state, actions] = makeHook()
+      const connectedProps = {
+        ...props,
+        [propName]: {
+          schema,
+          uiSchema,
+          ...state,
+          actions,
+        },
+      }
+      return <Component {...connectedProps} />
+    }
+  }
+
+  connect.Form = connect(ConfigForm)
+
+  return connect
+}
