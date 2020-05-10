24a9423f3fb576f1a9933b5d34d92f93dac339e8
chriscauley <chris@lablackey.com>
2020-05-10 22:45:21 +0000

    adding test for separate

--- __tests__/simulation.test.js
+++ __tests__/simulation.test.js
@@ -1,4 +1,5 @@
-import Simulation from '../client/simulation'
+import Simulation, { separate } from '../client/simulation'
+import { range } from 'lodash'
 
 const DEFAULTS = {
   people: 200,
@@ -36,4 +37,32 @@ test("Collide speed test", () => {
   while (i--) {
     simulation.collide(p0, p1, 0)
   }
+})
+
+test("Displacements", () => {
+  // I had a lot of trouble getting picese to stop spawning inside each other, so I'm shotgun testing it here
+  const getDistance = (p1, p2) => {
+    const dx2 = Math.pow(p1.x - p2.x, 2)
+    const dy2 = Math.pow(p1.y - p2.y, 2)
+    return Math.sqrt(dx2 + dy2)
+  }
+  const radius = 30
+  const cases = [
+    [100,100,110,100],
+    [100,100,100.001,110],
+    [100,100,110,110],
+    [100,100,90,90],
+  ]
+  range(100).forEach(() => {
+    cases.push([100,100,100+2*Math.random()*radius-radius,100+2*Math.random()*radius-radius])
+  })
+  cases.forEach(([x1, y1, x2, y2]) => {
+    const p1 = {x: x1, y: y1}
+    const p2 = {x: x2, y: y2}
+    const d1 = getDistance(p1, p2)
+    separate(p1, p2, radius)
+    const success = !Math.floor(60-getDistance(p1, p2).toFixed(2))
+    !success && console.log(success, getDistance(p1, p2).toFixed(2), x2.toFixed(0), y2.toFixed(0), success, x2 > 100, y2 > 100)
+    expect(success).toBe(true)
+  })
 })
