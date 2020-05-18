8607b3387fa5bea71406bfb4056f59d8521d182a
chriscauley <chris@lablackey.com>
2020-05-18 00:20:53 +0000

    moved cos to math repo

--- client/app.js
+++ client/app.js
@@ -5,7 +5,6 @@ import { HashRouter, Route } from 'react-router-dom'
 import ConfigForm from './ConfigForm'
 import Home from './Home'
 import Nav from './Nav'
-import Cos from './cos'
 
 const App = () => {
   return (
@@ -14,7 +13,6 @@ const App = () => {
         <Nav />
         <div className="app-content">
           <Route exact path="/" component={Home} />
-          <Route exact path="/cos/" component={Cos} />
         </div>
       </HashRouter>
       <ConfigForm />
